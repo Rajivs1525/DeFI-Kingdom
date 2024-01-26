9ec4a38862a5717579593ba68d808ccda4322954
Rajivs1525 <145263927+Rajivs1525@users.noreply.github.com>
2024-01-26 16:25:35 +0530

    Update Defi.sol

--- Defi.sol
+++ Defi.sol
@@ -4,8 +4,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
+    string public name = "Rajiv Srinivas";
+    string public symbol = "Rajivs2515";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
